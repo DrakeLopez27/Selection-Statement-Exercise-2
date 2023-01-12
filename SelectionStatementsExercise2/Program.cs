bacd434373b4160e9f3d801492431cc3cef1ffdf
DrakeLopez27 <121637335+DrakeLopez27@users.noreply.github.com>
2023-01-12 15:58:33 -0600

    finished Ex2

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,44 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your favorite school subject?");
+
+            string subject = Console.ReadLine();    
+
+            switch (subject.ToLower())
+            {
+                case "math":
+                    Console.WriteLine("Gross, I hate math!");
+                    break;
+
+                case "english":
+                    Console.WriteLine("Oh cool, yeah English is pretty easy.");
+                    break;
+
+                case "history":
+                    Console.WriteLine("I LOVE History!");
+                    break;
+
+                case "science":
+                    Console.WriteLine("Science is very cool.");
+                    break;
+
+                default:
+                    Console.WriteLine("I don't think I took that class.");
+                    break;
+
+            }
+
+
+
+
+
+
+
+
+
+
+
         }
     }
 }
